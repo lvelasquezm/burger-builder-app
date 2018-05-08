e1fbed478abf5c6dd643a4fdde0b4acd67551c04
Lawrence Velasquez <law.vzmk@gmail.com>
2018-05-08 10:43:29 -0600

    Move Layout component to hoc folder and updated references

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Aux from '../../../hoc/Auxiliar';
+import Aux from '../../../hoc/Auxiliar/Auxiliar';
 import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import classes from './Modal.css';
-import Aux from '../../../hoc/Auxiliar';
+import Aux from '../../../hoc/Auxiliar/Auxiliar';
 import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
