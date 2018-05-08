f48b13a45f6c9766399c4cc68388348ccf39801f
Lawrence Velasquez <law.vzmk@gmail.com>
2018-05-07 21:21:18 -0600

    Renamed Aux.js file to Auxiliar.js and change references

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Auxiliar';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
